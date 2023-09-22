9a9b69194625d855272e320d62ebf3dae32b127e
RafaelJosePortoSilva <rafaelporto.dev@gmail.com>
2023-09-22 11:46:24 -0300

    if adicionado

--- pages/home.py
+++ pages/home.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-df_list = [pd.read_csv('db/fifa/' + x) for x in os.listdir('db/fifa') if '.csv' in x]
 
-df = pd.concat(df_list)
-df.sort_values(by='ID',inplace=True)
-st.session_state['data'] = df
-df
+if 'data' not in st.session_state:
+    df_list = [pd.read_csv('db/fifa/' + x) for x in os.listdir('db/fifa') if '.csv' in x]
+    df = pd.concat(df_list)
+    df.sort_values(by='ID',inplace=True)
+    st.session_state['data'] = df
+    df
